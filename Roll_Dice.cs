06e9dcf55d5add347b922948d07fdde9349a68f9
Ryguy100 <124656432+Ryguy100@users.noreply.github.com>
2024-01-16 22:42:16 -0700

    Add project files.

---
+++ Roll_Dice.cs
@@ -0,0 +1,53 @@
+﻿using rolling_dice;
+using System.Numerics;
+
+internal class Roll_Dice
+{
+    private static void Main(string[] args)
+    {
+        Simulation sim = new Simulation();
+        
+        int rolls;
+        int[] results;
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("");
+
+        System.Console.Write("How many dice would you like to roll? ");
+        rolls = int.Parse(System.Console.ReadLine());
+
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls");
+        System.Console.WriteLine("Total number of rolls = " +  rolls);
+        System.Console.WriteLine("");
+
+
+        results = sim.Simulate(rolls); // Returns an array of the simulation
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            double stars;
+            int num;
+            
+            if (i == 0 | i == 1)
+            {
+                continue;
+            } 
+            else
+            {
+                num = results[i];
+                stars = Math.Round(((double)num / rolls) * 100);
+                string added_stars = "";
+                for (int j = 0; j < stars; j++)
+                {
+                    added_stars += '*';
+                }
+                System.Console.WriteLine(i + ": " + added_stars);
+            }
+        }
+        
+        System.Console.WriteLine("");
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    
+    }
+}
---
+++ Simulation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rolling_dice
+{
+    public class Simulation
+    {
+        public int[] Simulate(int rolls)
+        {
+            int[] output = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            
+            for (int i = 0; i < rolls; i++)
+            {
+                int rand1;
+                int rand2;
+                int sum;
+
+                Random rnd = new Random();
+
+                rand1 = rnd.Next(1, 7);
+                rand2 = rnd.Next(1, 7);
+
+                sum = rand1 + rand2;
+
+                output[sum] += 1;
+            }
+
+            return output;
+        }
+    }
+}
